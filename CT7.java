b5a1e15dbae67f1fd4688978a39880107fe21b80
parvez <pmosharaf366@gmail.com>
2024-05-22 00:49:55 +0600

    this is java code

---
+++ CT7.java
@@ -0,0 +1,18 @@
+
+
+
+
+ class CT7 {
+    public static int varM;
+    int varN;
+     void printValue(){
+        int varO=10;
+        System.out.println("varM:\t"+varM);
+        System.out.println("varN:\t"+varN);
+        System.out.println("varO:\t"+varO);
+    }
+    public static void main(String[] args) {
+        CT7 Object=new CT7();
+        Object.printValue();
+    }
+}
