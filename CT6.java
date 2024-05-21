907317c8b45d409f543e28c82c069188287edca1
parvez <pmosharaf366@gmail.com>
2024-05-22 01:07:13 +0600

    first java code

---
+++ CT6.java
@@ -0,0 +1,30 @@
+class Animal{
+    void makeSound(){
+        System.out.println("Animal");
+    }
+}
+class Dog extends Animal{
+    void makeSound(){
+        System.out.println("Dog");
+    }
+}
+class Cat extends Dog{
+    void makeSound(){
+        super.makeSound();
+    }
+}
+
+
+public class CT6 {
+    public static void main(String[] args) {
+        Animal a=new Animal();
+        Animal b=new Dog();
+        Dog c=new Cat();
+        
+
+        a.makeSound();
+        b.makeSound();
+        c.makeSound();
+    
+    }
+}
